04f36e224690ceefd97f2db03970e9502080811c
blitzarprogrammer <167507458+blitzarprogrammer@users.noreply.github.com>
2024-07-13 11:13:00 +0530

    Add files via upload

---
+++ Band_Name_Generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Genarator")
+City = input("Which City you grew up in?\n")
+Pet = input("What is the name of your pet?\n")
+print("Your band name could be:\n"+ City+" "+Pet)
+
